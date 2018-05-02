978d71d124fc52424b65baa4b384432adf3146e9
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2018-05-01 21:36:05 -0500

    Ajustes

--- arrays_creating_news_from_old.pl
+++ arrays_creating_news_from_old.pl
@@ -6,3 +6,9 @@ my @elements = ("Antimonio", "Arsenic", "Aluminum", "Selenium");
 say @elements; #AntimonioArsenicAluminumSelenium
 say "@elements"; #Antimonio Arsenic Aluminum Selenium 
 say join (", ", @elements); #Antimonio, Arsenic, Aluminum, Selenium
+
+say reverse ("Hello", "World"); #WorldHello
+say reverse ("HelloWorld"); #HelloWorld
+say scalar reverse("HelloWorld"); #dlroWolleH
+say scalar reverse ("Hello", "World"); #dlroWolleH
+
